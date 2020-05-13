a6eed69ba963d856de029df3f8be07604d2a4190
tatsuya828828 <kkjy011894@gmail.com>
2020-05-13 16:44:19 +0000

    検索機能の追加

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,13 @@
 class SearchesController < ApplicationController
+
+	def index
+		@range = params[:range]
+		@word = params[:word]
+		if @range == "グループID"
+			@group = Group.find_by(self_id: @word)
+			@leader = User.find_by(id: @group.leader)
+		else
+			@user = User.find_by(self_id: @word)
+		end
+	end
 end
