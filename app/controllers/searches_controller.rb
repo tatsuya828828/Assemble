315e67b32ebfa2fd2a9a39eb3371a913452280d0
tatsuya828828 <kkjy011894@gmail.com>
2020-05-16 15:59:41 +0000

    検索機能の使用を変更

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,12 +4,22 @@ class SearchesController < ApplicationController
 		@range = params[:range]
 		@word = params[:word]
 		if @range == "グループID"
-			@group = Group.find_by(self_id: @word)
+			@group = Group.find_by(self_id: @word, private_status: "open")
 			if @group.present?
 				@leader = User.find_by(id: @group.leader)
 			end
 		else
 			@user = User.find_by(self_id: @word)
+
+			if (friend = Friend.find_by(sender_id: current_user.id, receiver_id: @user.id)).present?
+				if friend.request_status == "friend"
+				@friend = friend
+				elsif friend.request_status == "waiting_for_allow"
+				@friend = friend
+				end
+			else
+				@friend = Friend.find_by(sender_id: @user.id, receiver_id: current_user.id, request_status: "waiting_for_allow")
+			end
 		end
 		if @group.nil? && @user.nil?
 			@range = nil
