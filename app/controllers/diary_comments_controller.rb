a58df4862aec062e3ed4074e8a15f93d3c0cbb91
tatsuya828828 <kkjy011894@gmail.com>
2020-05-16 18:15:34 +0000

    日記コメント機能の追加

--- app/controllers/diary_comments_controller.rb
+++ app/controllers/diary_comments_controller.rb
@@ -1,2 +1,23 @@
 class DiaryCommentsController < ApplicationController
+
+	def create
+		group = params[:group_id]
+		comment = DiaryComment.new(diary_comment_params)
+		comment.save
+		binding.pry
+		redirect_to group_diary_path(group_id: group, id: comment.diary_id)
+	end
+
+	def destroy
+		comment = DiaryComment.find(params[:id])
+		comment.save
+		redirect_back(fallback_location: root_path)
+	end
+
+	private
+
+	def diary_comment_params
+		params.permit(:comment, :user_id, :diary_id)
+	end
+
 end
