f614b0100e08cf3099a2604da184028d636bcb4b
tatsuya828828 <kkjy011894@gmail.com>
2020-05-17 15:47:51 +0000

    管理者の作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,18 @@
 class Admin::UsersController < ApplicationController
+	before_action :authenticate_admin!
+
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def update
+		user = User.find(params[:id])
+		user.update(valid_status: params[:status])
+		redirect_back(fallback_location: admin_root_path)
+	end
+
 end
