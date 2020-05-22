7e5dd55fb647fbde06713f4340f63a56c765a86e
tatsuya828828 <kkjy011894@gmail.com>
2020-05-22 18:34:56 +0000

    adminにユーザー検索機能追加

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,4 +1,10 @@
 class Admin::SearchesController < ApplicationController
   def index
+  	@word = params[:word]
+  	if params[:range] == "ユーザー名"
+  		@users = User.where(name: params[:word])
+  	else
+  		@user = User.find_by(self_id: params[:word])
+  	end
   end
 end
