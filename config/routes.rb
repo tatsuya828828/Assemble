72ba9a2bc556972c6c54ceef799806ebee7af6a7
tatsuya828828 <kkjy011894@gmail.com>
2020-05-25 04:01:22 +0000

    actioncableのための記述追加

--- config/routes.rb
+++ config/routes.rb
@@ -73,6 +73,5 @@ Rails.application.routes.draw do
   get '/searches', to: 'searches#index', as: 'searches'
 #=====================================================================================
 
-mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
