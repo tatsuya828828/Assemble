fce190db0d3bcc15f43384aefd38bfbe45573aed
tatsuya828828 <kkjy011894@gmail.com>
2020-05-27 11:20:07 +0000

    s3の画像が表示されない不具合の修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,15 @@
+# CarrierWaveの設定呼び出し
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
+# 画像名に日本語が使えるようにする
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+
+# 保存先の分岐
 CarrierWave.configure do |config|
-  if Rails.env.production? || Rails.env.development? # 開発中もs3使う
+  # 本番環境はS3に保存
+  if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'our-space-image'
@@ -14,8 +20,8 @@ CarrierWave.configure do |config|
       aws_access_key_id: ENV['AWS_ACCESS_ID'],
       aws_secret_access_key: ENV['AWS_ACCESS_KEY'],
       region: 'ap-northeast-1',
-      path_style: true
     }
+  # 開発環境はlocalに保存
   else
     config.storage :file
     config.enable_processing = false if Rails.env.test?
