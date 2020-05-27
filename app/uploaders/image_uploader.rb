e02fab7b634e4490e240c1649500c07f1fffb101
tatsuya828828 <kkjy011894@gmail.com>
2020-05-27 11:03:02 +0000

    s3の画像が表示されない不具合の修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,9 +36,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
+  version :thumb do
      process resize_to_fit: [250, 250]
-  # end
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,12 +8,13 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'our-space-image'
     config.asset_host = 'https://our-space-image.s3-ap-northeast-1.amazonaws.com'
+    config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_ID'],
       aws_secret_access_key: ENV['AWS_ACCESS_KEY'],
       region: 'ap-northeast-1',
-      # path_style: true
+      path_style: true
     }
   else
     config.storage :file
