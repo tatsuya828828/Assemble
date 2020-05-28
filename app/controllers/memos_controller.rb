ca34d60a697976af5835c57fe395ad508328bced
tatsuya828828 <kkjy011894@gmail.com>
2020-05-28 08:11:05 +0000

    メモ通知機能の追加

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -4,6 +4,16 @@ class MemosController < ApplicationController
 	def create
 		memo = Memo.new(memo_params)
 		memo.save
+
+		# メモを貼ったときにグループのユーザーへ通知
+		group = Group.find_by(id: memo.group_id)
+		group.users.each do |user|
+			if user != current_user
+				notification = Notification.new(creator_id: memo.user_id, group_id: memo.group_id, memo_id: memo.id, confirm_status: "unconfirmed")
+				notification.confirmer_id = user.id
+				notification.save
+			end
+		end
 		redirect_to group_path(memo.group_id)
 	end
 
