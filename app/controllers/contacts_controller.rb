418af8f5be70e0d4755eee793ef00ac6e478e00b
tatsuya828828 <kkjy011894@gmail.com>
2020-06-04 05:38:38 +0000

    問い合わせコントローラーの作成

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,27 @@
+class ContactsController < ApplicationController
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    contact = contact.new(contact_params)
+    contact.save
+    redirect_to action: :index
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def index
+    @contacts = current_user.contacts.all
+  end
+
+  def destroy
+    contact = Contact.find(params[:id])
+    contact.destroy
+    redirect_to action: :index
+  end
+
+end
