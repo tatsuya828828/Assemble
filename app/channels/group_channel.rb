0236c14257f7ff53124ed6dfb5c0ffb0d282003c
tatsuya828828 <kkjy011894@gmail.com>
2020-06-11 11:07:54 +0000

    メッセージを空で投稿した際に通知のみ空で投稿される不具合の修正

--- app/channels/group_channel.rb
+++ app/channels/group_channel.rb
@@ -9,14 +9,15 @@ class GroupChannel < ApplicationCable::Channel
 
   def speak(content)
   	@new_message = Message.new(content: content['content'][0], user_id: content['content'][1].to_i, group_id: content['content'][2].to_i)
-  	@new_message.save
+  	if @new_message.save
 
-    # DMを送ったグループユーザーに通知を送る
-    @new_message.group.users.each do |user|
-      if user != @new_message.user
-        notification = Notification.new(creator_id: @new_message.user_id, message_id: @new_message.id, group_id: @new_message.group_id, confirm_status: "unconfirmed")
-        notification.confirmer_id = user.id
-        notification.save
+      # DMを送ったグループユーザーに通知を送る
+      @new_message.group.users.each do |user|
+        if user != @new_message.user
+          notification = Notification.new(creator_id: @new_message.user_id, message_id: @new_message.id, group_id: @new_message.group_id, confirm_status: "unconfirmed")
+          notification.confirmer_id = user.id
+          notification.save
+        end
       end
     end
   	ActionCable.server.broadcast 'group_channel', content: content['content'][0], user_name: @new_message.user.name, user_id: @new_message.user.id, group_id: @new_message.group.id, time: @new_message.created_at.strftime("%-H:%M")
--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -9,14 +9,15 @@ class RoomChannel < ApplicationCable::Channel
 
   def speak(content)
   	@new_direct_message = DirectMessage.new(content: content['content'][0], user_id: content['content'][1].to_i, room_id: content['content'][2].to_i)
-    @new_direct_message.save
+    if @new_direct_message.save
 
-    # DMを送った相手に通知を送る
-    @new_direct_message.room.users.each do |user|
-      if user != @new_direct_message.user
-        notification = Notification.new(creator_id: @new_direct_message.user_id, direct_message_id: @new_direct_message.id, confirm_status: "unconfirmed", room_id: @new_direct_message.room_id)
-        notification.confirmer_id = user.id
-        notification.save
+      # DMを送った相手に通知を送る
+      @new_direct_message.room.users.each do |user|
+        if user != @new_direct_message.user
+          notification = Notification.new(creator_id: @new_direct_message.user_id, direct_message_id: @new_direct_message.id, confirm_status: "unconfirmed", room_id: @new_direct_message.room_id)
+          notification.confirmer_id = user.id
+          notification.save
+        end
       end
     end
   	ActionCable.server.broadcast 'room_channel', content: content['content'][0], user_name: @new_direct_message.user.name, user_id: @new_direct_message.user.id, room_id: @new_direct_message.room.id, time: @new_direct_message.created_at.strftime("%-H:%M")
